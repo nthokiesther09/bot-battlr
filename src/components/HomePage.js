fc9713ea6392cf9c9ee92ecb98fd83938410df27
nthokiesther09 <nthokiesther09@gmail.com>
2023-01-27 16:45:29 +0300

    modified components and package.json

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -15,13 +15,13 @@ function HomePage () {
     setArmy((army) => army.filter((it) => it.id !== bot.id));
   }
   useEffect(() => {
-    fetch("https://battlr-data-trial.vercel.app/bots")
+    fetch("https://json-server-vercel-lilac.vercel.app/bots")
       .then((res) => res.json())
       .then((data) => setBots(data));
   }, []);
 
   function handleDelete(bot) {
-    fetch(`https://battlr-data-trial.vercel.app/bots/${bot.id}`, {
+    fetch(`https://json-server-vercel-lilac.vercel.app/bots/${bot.id}`, {
       method: "DELETE",
     }).then(() => {
       setBots((bots) => bots.filter((it) => it.id !== bot.id));
